10afd82e0e0add48a81fb0f11dc795ff1b833fae
shehan <s.tharuka1998@gmail.com>
2021-07-08 22:07:27 -0700

    Sensor data chart

--- sensor/src/Screens/SensorChart.js
+++ sensor/src/Screens/SensorChart.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
-import './sensorChart.css'
+import '../index.css'
 
 class SensorChart extends Component{
     constructor(props){
